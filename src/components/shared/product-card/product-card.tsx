8a212e6563eeeab04f23348e9c3db588600f7d1b
vatiza <rakibur.mac@gmail.com>
2025-01-29 23:16:50 +0600

    deploy test

--- src/components/shared/product-card/product-card.tsx
+++ src/components/shared/product-card/product-card.tsx
@@ -17,7 +17,7 @@ const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
   const { title, image, price } = product;
 
   return (
-    <div className="group relative w-[250px]  h-[278px] bg-white rounded-lg overflow-hidden shadow-sm hover:shadow-lg transition-shadow duration-300 mt-5">
+    <div className="group relative w-[250px] border  h-[278px] bg-white rounded-lg overflow-hidden shadow-sm hover:shadow-lg transition-shadow duration-300 mt-5">
       {/* Image Container */}
       <div className="relative h-[170px]  overflow-hidden">
         <Image
@@ -26,7 +26,7 @@ const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
           width="0"
           height="0"
           sizes="100vw"
-          className="object-cover w-full h-auto mx-auto transition-transform duration-700 group-hover:scale-110"
+          className="object-cover w-full h-auto  transition-transform duration-700 group-hover:scale-110"
         />
 
         {/* Featured Badge */}
